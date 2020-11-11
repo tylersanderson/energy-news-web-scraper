6659fa451ba226fda707267eb4e15c630a71d7d0
tylersanderson <tylersanderson@hotmail.com>
2020-11-10 19:59:24 -0600

    program now returns a dictionary containing all article titles and links for all articles at energycentral.com/news

--- main.py
+++ main.py
@@ -1,28 +1,35 @@
 import requests
 import time
 from bs4 import BeautifulSoup
-from selenium import webdriver
+from selenium.webdriver import Chrome
+from selenium.webdriver.chrome.options import Options
 import pprint
 
-
 url = "https://energycentral.com/news"
-browser = webdriver.Chrome()
-browser.get(url)
-# time.sleep(30)
-html = browser.page_source
 
-soup = BeautifulSoup(html, 'lxml')
-#soup2 = BeautifulSoup(res2.text, 'html.parser')
-links = soup.select('.article-title-link')
-title = soup.select('.article-title')
-main = soup.select('.content')
-#links2 = soup2.select('.storylink')
-#subtext2 = soup2.select('.subtext')
+chrome_options = Options()
+chrome_options.add_argument("--headless")  # Opens the browser up in background
 
-mega_links = links  # + links2
-# mega_subtext = subtext  # + subtext2
+with Chrome(options=chrome_options) as browser:
+    browser.get(url)
+    html = browser.page_source
 
+page_soup = BeautifulSoup(html, 'html.parser')
 
-#pprint.pprint(create_custom_hn(links, title))
-pprint.pprint(soup)
-# print(soup)
+links_list = []
+for i in page_soup.find_all('span', {'class': 'article-title'}):
+    link = i.find('a', href=True)
+    if link is None:
+        continue
+    links_list.append(link['href'])
+
+title_list = []
+for i in page_soup.find_all('span', {'class': 'article-title'}):
+    title = i.find('span')
+    if title is None:
+        continue
+    title_list.append(i.getText())
+
+title_link_dict = dict(zip(title_list, links_list))
+
+pprint.pprint(title_link_dict)
